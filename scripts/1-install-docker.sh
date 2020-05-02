f92e5239c2f3dc3dcea9a00493decf3cc5f58fd2
jittagorn pitakmetagoon <jittagornp@gmail.com>
2020-05-02 19:54:58 +0700

    Update 1-install-docker.sh

--- scripts/1-install-docker.sh
+++ scripts/1-install-docker.sh
@@ -8,5 +8,3 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo docker --version
-
-
